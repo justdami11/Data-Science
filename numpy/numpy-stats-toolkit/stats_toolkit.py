21a366c02811ac015fb676c66a960f4105b1a0a7
JDM <justindamitosa1105@gmail.com>
2025-07-25 22:37:22 -0600

    create the numpy's  stats toolkit and the function mean

---
+++ numpy/numpy-stats-toolkit/stats_toolkit.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def mean(x):
+    total = np.sum(x)
+    count = x.size
+    return (total/count)
+
+def population_variance(x):
+    mean = np.mean(x)
+    squared_diff = (x - mean) ** 2
+    variance = np.sum(squared_diff) / x.size
+    return variance
+
+def sample_variance(x):
+    mean = np.mean(x)
+    squared_diffs = (x - mean) ** 2
+    variance = np.sum(squared_diffs) / (x.size - 1)
+    return variance
